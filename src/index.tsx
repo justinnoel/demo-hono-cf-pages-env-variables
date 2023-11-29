aee41649f89fb43d5d73c28431093ccc0266d72a
Justin <81643826+justinnoel@users.noreply.github.com>
2023-11-29 11:09:20 -0600

    test: attempt to access environment variables

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ const app = new Hono()
 app.get('*', renderer)
 
 app.get('/', (c) => {
+  console.log("c.env"); console.log(JSON.stringify(c.env, null, 3));
+
   return c.render(<h1>Hello!</h1>)
 })
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,19 @@ import devServer from '@hono/vite-dev-server'
 import pages from '@hono/vite-cloudflare-pages'
 
 export default defineConfig({
+  define: {
+    'process.env': process.env
+  },
   plugins: [
     pages(),
     devServer({
-      entry: 'src/index.tsx'
+      entry: 'src/index.tsx',
+      cf: {
+        bindings: {
+          TEST_VARIABLE: process.env.TEST_VARIABLE ?? '',
+          VITE_TEST_VARIABLE: process.env.VITE_TEST_VARIABLE ?? '',
+        },
+      },
     })
   ]
 })
