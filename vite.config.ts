9b457580e244086f3df5c7e49ac6e699cc6dc98e
Justin <81643826+justinnoel@users.noreply.github.com>
2023-11-29 17:55:16 -0600

    fix: correct the importing of .env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import devServer from '@hono/vite-dev-server'
 import pages from '@hono/vite-cloudflare-pages'
 
-export default defineConfig({
-  define: {
-    'process.env': process.env
-  },
-  plugins: [
-    pages(),
-    devServer({
-      entry: 'src/index.tsx',
-      cf: {
-        bindings: {
-          TEST_VARIABLE: process.env.TEST_VARIABLE ?? '',
-          VITE_TEST_VARIABLE: process.env.VITE_TEST_VARIABLE ?? '',
-          TEST_HARD_CODED: "hardcoded"
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      pages(),
+      devServer({
+        entry: 'src/index.tsx',
+        cf: {
+          bindings: {
+            TEST_VARIABLE: env.TEST_VARIABLE ?? '',
+            VITE_TEST_VARIABLE: env.VITE_TEST_VARIABLE ?? '',
+            TEST_HARD_CODED: "hardcoded"
+          },
         },
-      },
-    })
-  ]
+      })
+    ]
+  
+  }
 })
